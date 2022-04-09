598f111a010b47d4c372970899acda314639b98d
Soonyub Hwang <nowonbun@naver.com>
2022-04-09 09:06:03 +0900

    initialize

---
+++ SourceCopy/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SourceCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(313, 19);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Source Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(313, 19);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Destiny Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(313, 19);
+            this.textBox3.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(331, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Copy List";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 105);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(427, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Execute";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 140);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ SourceCopy/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SourceCopy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            textBox1.Text = @"D:\SCP\Work";
+            textBox2.Text = @"D:\SCP\Merge";
+            textBox3.Text = @"D:\SCP\copylist.txt";
+
+            button1.Click += (sender, agrs) =>
+            {
+                var dialog1 = new FolderBrowserDialog();
+                var result = dialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    textBox1.Text = dialog1.SelectedPath;
+                }
+            };
+            button2.Click += (sender, agrs) =>
+            {
+                var dialog2 = new FolderBrowserDialog();
+                var result = dialog2.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    textBox2.Text = dialog2.SelectedPath;
+                }
+            };
+            button3.Click += (sender, args) =>
+            {
+                var dialog3 = new OpenFileDialog();
+                var result = dialog3.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    textBox3.Text = dialog3.FileName;
+                }
+            };
+            button4.Click += (sender, args) =>
+            {
+                String filepath = textBox3.Text;
+                FileInfo info = new FileInfo(filepath);
+                if (!info.Exists)
+                {
+                    MessageBox.Show("not list");
+                    return;
+                }
+                if (!Directory.Exists(textBox1.Text))
+                {
+                    MessageBox.Show("not directory");
+                    return;
+                }
+                using (FileStream stream = info.OpenRead())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            String str = reader.ReadLine().Trim();
+                            if (String.IsNullOrEmpty(str.Trim())) {
+                                continue;
+                            }
+                            String des = str.Replace(textBox1.Text, textBox2.Text);
+                            FileInfo co = new FileInfo(des);
+                            if (!co.Directory.Exists)
+                            {
+                                co.Directory.Create();
+                            }
+
+                            FileInfo aa = new FileInfo(str);
+                            if (!aa.Exists)
+                            {
+                                throw new Exception("file nothings");
+                            }
+                            try
+                            {
+                                aa.CopyTo(co.FullName, true);
+                            }catch(Exception ex)
+                            {
+                                MessageBox.Show(ex.ToString());
+                            }
+                        
+                            //File.Copy(str, des, true);
+                        }
+                        MessageBox.Show("complete");
+                    }
+                }
+            };
+        }
+    }
+}
